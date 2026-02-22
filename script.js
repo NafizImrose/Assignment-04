78f3bbf4453a184e8ef18640e6d1ad76e597710d
NafizImrose <nafizimrose.official@gmail.com>
2026-02-23 00:41:41 +0600

    Add interview and reject button functionality with status updates

--- script.js
+++ script.js
@@ -10,3 +10,92 @@ function showOnly(value) {
   const select = document.getElementById(value);
   select.classList.remove("hidden");
 }
+
+function interviewButton(button) {
+  const interviewCount = document.getElementById("interview");
+  let number = Number(interviewCount.innerText);
+
+  if (number == 0) {
+    alert("no number");
+  }
+
+  const section = document.getElementById("interview-section");
+  const card = button.closest(".card");
+
+  const statusBox = card.querySelector(".w-29");
+  const text = statusBox.querySelector("p");
+
+  //   checking for duplicate append
+  if (text.innerText == "APPLIED") {
+    return;
+  }
+
+  if (text.innerText == "REJECTED") {
+    const rejectedCount = document.getElementById("rejected");
+    let number = Number(rejectedCount.innerText);
+    number = number - 1;
+    rejectedCount.innerText = number;
+    statusBox.classList.remove("bg-red-500");
+  }
+
+  if ((text.innerText = "NOT APPLIED")) {
+    statusBox.classList.remove("bg-slate-300");
+  }
+
+  if (section && card) {
+    const copy = card.cloneNode(true);
+    section.append(copy);
+    number = number + 1;
+    interviewCount.innerText = number;
+  }
+
+  text.innerText = "APPLIED";
+
+  statusBox.classList.add("bg-green-500");
+  statusBox.classList.add("text-white");
+}
+
+function rejectButton(button) {
+  const rejectedCount = document.getElementById("rejected");
+  let number = Number(rejectedCount.innerText);
+
+  if (number == 0) {
+    alert("no number");
+  }
+
+  const section = document.getElementById("rejected-section");
+  const card = button.closest(".card");
+
+  const statusBox = card.querySelector(".w-29");
+  const text = statusBox.querySelector("p");
+
+  //   checking for duplicate append
+  if (text.innerText == "REJECTED") {
+    return;
+  }
+
+  if (text.innerText == "APPLIED") {
+    const interviewCount = document.getElementById("interview");
+    let number = Number(interviewCount.innerText);
+    number = number - 1;
+    interviewCount.innerText = number;
+    statusBox.classList.remove("bg-green-500");
+  }
+
+  if ((text.innerText = "NOT APPLIED")) {
+    statusBox.classList.remove("bg-slate-300");
+  }
+
+  if (section && card) {
+    const copy = card.cloneNode(true);
+    section.append(copy);
+    number = number + 1;
+    rejectedCount.innerText = number;
+  }
+
+  text.innerText = "REJECTED";
+
+  statusBox.classList.remove("bg-slate-300");
+  statusBox.classList.add("bg-red-500");
+  statusBox.classList.add("text-white");
+}
